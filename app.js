d68e26181b539e8aa9805279411b946c1000f223
hyeonQyu <dhk0561@naver.com>
2020-03-19 21:56:51 +0900

    express를 통한 정적 파일 서비스

--- app.js
+++ app.js
@@ -2,19 +2,22 @@
 var express = require('express');
 var app = express();
 
+// 정적인 파일을 public 디렉토리에 넣고 다음 코드를 추가
+app.use(express.static('public'));
+
 // 루트
 app.get('/', function(req, res){
   res.send('Hello home page');
 });
 
-app.get('/unity', function(req, res){
-  res.send('Hello Unity, <img src="/unity.png">');
-})
-
 // html을 사용할 수 있음
 app.get('/login', function(req, res){
   res.send('<h1>Please login</h1>');
-})
+});
+
+app.get('/unity', function(req, res){
+  res.send('Hello Unity, <img src="/unity.png">');
+});
 
 // port 번호를 이용하여 listen
 app.listen(3000, function(){
