80d225fea7861bb4600d1070f950870fb4845d1f
hyeonQyu <dhk0561@naver.com>
2020-03-19 22:13:24 +0900

    동적인 파일과 정적인 파일을 사용한 웹페이지 표현

--- app.js
+++ app.js
@@ -19,6 +19,35 @@ app.get('/unity', function(req, res){
   res.send('Hello Unity, <img src="/unity.png">');
 });
 
+app.get('/dynamic', function(req, res){
+  var lis = '';
+  for(var i = 0; i < 5; i++){
+    lis = lis + '<li>coding</li>';
+  }
+
+  // 시간 출력(정적인 파일로는 시간을 출력할 수 없음)
+  var time = Date();
+
+  // 동적 페이지에 html을 사용
+  var output = `
+  <<!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      Hello, dynamic
+      <ul>
+        ${lis}
+      </ul>
+      ${time}
+    </body>
+  </html>`
+
+  res.send(output);
+});
+
 // port 번호를 이용하여 listen
 app.listen(3000, function(){
   console.log('Connected 3000 port');
